9e86a8b74631f2dd3cf09a8aff41b35def40a25f
pogeku <pogeku@lapiz>
2026-05-11 14:08:43 -0300

    adding a backend for the project echarts

---
+++ backends/echarts-sample/src/main/java/com/tom/echarts/sample/EchartsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.tom.echarts.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchartsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchartsSampleApplication.class, args);
+	}
+
+}
---
+++ backends/echarts-sample/src/test/java/com/tom/echarts/sample/EchartsSampleApplicationTests.java
@@ -0,0 +1,5 @@
+package com.tom.echarts.sample;
+
+class EchartsSampleApplicationTests {
+
+}
